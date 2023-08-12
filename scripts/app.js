3b93a7d06ebc296659acac56c2ed4b884c4ad01e
Sebastian <mrseb10@gmail.com>
2023-08-13 00:53:42 +0200

    Page loading on start

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,15 @@
+// Renders the page on start
+getData()
+
 const periodSelectors = document.querySelectorAll('.period');
 // adds toggle to each clicked button
 periodSelectors.forEach(button => {
   button.addEventListener('click', () => {
+    // renders content on the page (probably had to add some parameters)
+    getData()
     // first removes toggle from other buttons
     toggleOff()
+    
     button.classList.add('is-toggled');
   })
 })
--- scripts/dom_data.js
+++ scripts/dom_data.js
@@ -1,27 +1,90 @@
-fetch('scripts/data.json')
+const getData = () => { fetch('scripts/data.json')
   .then(res => res.json())
   .then(data => {
     const placeholder = document.querySelector('.right-side');
-
-    data.forEach(activity => {
-      placeholder.innerHTML += `
-      <div class="container">
-        <div class="color-header play"></div>
-        <div class="time-info">
-          <div class="time-header">
-            <div class="activity-name">
-            ${activity.title}
+    placeholder.innerHTML = ``;
+    
+      data.forEach(activity => {
+        if (periodSelectors[0].classList.contains('is-toggled')) {
+          placeholder.innerHTML += `
+        <div class="container">
+          <div class="color-header play"></div>
+          <div class="time-info">
+            <div class="time-header">
+              <div class="activity-name">
+              ${activity.title}
+              </div>
+              <img class="options-button" src="images/icon-ellipsis.svg" alt="">
             </div>
-            <img class="options-button" src="images/icon-ellipsis.svg" alt="">
-          </div>
-          <div class="time-amount">
-          ${activity.timeframes.weekly.current}hrs
-          </div>
-          <div class="time-before">
-          Last week - ${activity.timeframes.weekly.previous}hrs
+            <div class="time-amount">
+            ${activity.timeframes.daily.current}hrs
+            </div>
+            <div class="time-before">
+            Yesterday - ${activity.timeframes.daily.previous}hrs
+            </div>
           </div>
         </div>
-      </div>
-      `
+        `
+        } else if (periodSelectors[1].classList.contains('is-toggled')) {
+          placeholder.innerHTML += `
+        <div class="container">
+          <div class="color-header play"></div>
+          <div class="time-info">
+            <div class="time-header">
+              <div class="activity-name">
+              ${activity.title}
+              </div>
+              <img class="options-button" src="images/icon-ellipsis.svg" alt="">
+            </div>
+            <div class="time-amount">
+            ${activity.timeframes.weekly.current}hrs
+            </div>
+            <div class="time-before">
+            Last week - ${activity.timeframes.weekly.previous}hrs
+            </div>
+          </div>
+        </div>
+        `
+        } else if (periodSelectors[2].classList.contains('is-toggled')) {
+          placeholder.innerHTML += `
+          <div class="container">
+            <div class="color-header play"></div>
+            <div class="time-info">
+              <div class="time-header">
+                <div class="activity-name">
+                ${activity.title}
+                </div>
+                <img class="options-button" src="images/icon-ellipsis.svg" alt="">
+              </div>
+              <div class="time-amount">
+              ${activity.timeframes.monthly.current}hrs
+              </div>
+              <div class="time-before">
+              Last month - ${activity.timeframes.monthly.previous}hrs
+              </div>
+            </div>
+          </div>
+          `
+        } else {
+          placeholder.innerHTML += `
+          <div class="container">
+            <div class="color-header play"></div>
+            <div class="time-info">
+              <div class="time-header">
+                <div class="activity-name">
+                ${activity.title}
+                </div>
+                <img class="options-button" src="images/icon-ellipsis.svg" alt="">
+              </div>
+              <div class="time-amount">
+              -- hrs
+              </div>
+              <div class="time-before">
+              </div>
+            </div>
+          </div>
+          `
+        };
+      });
     });
-  });
+  }
